5b15c9d1598e90917bc6bd6b5835965d9da5605d
赵建 <Z@MacBook-Pro.local>
2023-02-24 23:49:03 +1300

    Revert "Revert "finish API development""
    This reverts commit 12d89fec3a218110906a013b140c3206188bc554.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        // we are going to use dependency injection to get our store context inside here so that wwe've got access to the products table in our database
+        // In order to use dependency injection, we create a private field inside our class and assign that private fields to the context that we're adding in our constructor here
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        // we are going to return a list of products from this method, but what we typically do inside an API controller is specify the type of result we're returning, so we can return the type of ActionResult
+        public async Task<ActionResult<List<Product>>> GetProducts() // use async then need Task to wrap the ActionResult
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // api/products/3
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+
+    }
+}
