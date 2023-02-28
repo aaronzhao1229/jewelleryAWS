6b3e65382f01fd2832697f2c754287c1bb0998a4
赵建 <Z@MacBook-Pro.local>
2023-02-28 17:05:04 +1300

    end of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,7 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 
@@ -27,7 +28,7 @@ function App() {
           <CssBaseline />
           <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
           <Container>
-              <Catalog />
+              <Outlet />
           </Container>
           
       </ThemeProvider>
